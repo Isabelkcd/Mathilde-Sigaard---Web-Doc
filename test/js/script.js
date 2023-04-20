d0043159485beaddfa5dc49cb44fc378ad834733
Isabelkcd <112545381+Isabelkcd@users.noreply.github.com>
2023-04-20 14:27:17 +0200

    Add files via upload

---
+++ test/js/script.js
@@ -0,0 +1,52 @@
+document.getElementById("left").addEventListener("click", independent);
+
+function independent() {
+    window.open("siglund.html", "_self");
+}
+
+document.getElementById("right").addEventListener("click", student);
+
+function student() {
+    window.open("personal.html", "_self");
+}
+
+
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+
+
+  function revealSlow() {
+    var reveals = document.querySelectorAll(".reveal-slow");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", revealSlow);
